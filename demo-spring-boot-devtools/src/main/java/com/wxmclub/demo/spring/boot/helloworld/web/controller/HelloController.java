21fcac75d878aefae89684c99509aec235990b39
wxmclub <wxmclub@gmail.com>
2018-01-17 13:58:57 +0800

    first commit

---
+++ demo-spring-boot-devtools/src/main/java/com/wxmclub/demo/spring/boot/helloworld/web/controller/HelloController.java
@@ -0,0 +1,42 @@
+package com.wxmclub.demo.spring.boot.helloworld.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 注：修改类内容后，IDEA环境需要手动编译才能生效
+ *
+ * @author wxmclub@gmail.com
+ * @version 1.0
+ * @date 2017-12-17
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/say")
+    public String say() {
+        System.out.println("say: test");
+        return "this is test message!";
+    }
+
+    @RequestMapping("/demo")
+    public Map<String, Object> getDemo() throws ParseException {
+        Map<String, Object> demo = new LinkedHashMap<>();
+        demo.put("id", 1000L);
+        demo.put("name", "tom");
+        demo.put("sex", 1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        demo.put("birthday", sdf.parse("1990-11-05"));
+        demo.put("remark", "测试中文");
+        demo.put("createDate", new Date());
+        return demo;
+    }
+
+}
---
+++ demo-spring-boot-helloworld/src/main/java/com/wxmclub/demo/spring/boot/helloworld/web/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.wxmclub.demo.spring.boot.helloworld.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author wxmclub@gmail.com
+ * @version 1.0
+ * @date 2017-12-17
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/say")
+    public String say() {
+        System.out.println("say: test");
+        return "this is test message!";
+    }
+
+    @RequestMapping("/demo")
+    public Map<String, Object> getDemo() throws ParseException {
+        Map<String, Object> demo = new LinkedHashMap<>();
+        demo.put("id", 1004L);
+        demo.put("name", "jack");
+        demo.put("sex", 0);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        demo.put("birthday", sdf.parse("1990-10-05"));
+        demo.put("remark", "测试中文123");
+        demo.put("createDate", new Date());
+        return demo;
+    }
+
+}
